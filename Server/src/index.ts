2d3c04b5fce55daf85857f520856e7d9abd59b29
William <william.hagg@effectsoft.se>
2025-11-20 21:57:00 +0100

    Added node to my server part of my portfolio. Installed express, axios, cors, and dotenv for the server side duo to token security, and fetching my projects from my github api with a faster response time and also more secure. Installed typescript to the lib. made some changes to the config file and package.json

---
+++ Server/src/index.ts
@@ -0,0 +1,8 @@
+import express = require('express');
+import axios = require ('axios');
+import dotenv = require ('dotenv');
+import cors = require ('cors');
+
+dotenv.config();
+
+const app = express();
