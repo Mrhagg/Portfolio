600f2a3be56dfa2f15f73acb1790aa1e73052b15
William <william.hagg@effectsoft.se>
2025-11-29 10:27:10 +0100

    fix

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import { Link as RouterLink, useLocation} from "react-router-dom"
 import { Menu,X } from "lucide-react";
