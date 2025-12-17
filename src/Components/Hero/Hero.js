2896737984098a147f0ffd79275d637ea2a66d2a
William <william.hagg@effectsoft.se>
2025-12-17 18:12:56 +0100

    Added some css changes to contact page to make it more responsive when u send a message from the contact-form. Also added fixes to the project page sliders and made it more central when u click on the arrows in mobile view. Updated CV for hero section download.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -19,7 +19,7 @@ function Hero() {
         <div className="cert">
             <div className="download-link">
               <p>If you are interested in my work and want to know more about me, <strong>Click here to download my CV</strong></p>
-              <a className="btn btn-danger" href="CV-William Hägg-EN.pdf" download="CV-William Hägg-EN.pdf">Download<FontAwesomeIcon className="icon" icon={faFile} /></a>
+              <a className="btn btn-danger" href="CV-William Hägg-ENGLISH.pdf" download="CV-William Hägg-ENGLISH.pdf">Download<FontAwesomeIcon className="icon" icon={faFile} /></a>
             </div>
         </div>
        
